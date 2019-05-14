e0c7aa4f48610733843ba159e37cc251b7e27fba
Chaudhari <C5076473@acsind.com>
2019-05-14 18:30:22 +0530

    Models Question & Answer created.
    New features added -
    1. Two new django models created -
    	Question
    	Answer
    2. Django administration superuser 'admin' created.
    
    Changes -
    1. Registered application 'canvass' in settings.py
    2. Registered models - Question, Answer in admin.py
    
    #Issues: 0

--- canvass/models.py
+++ canvass/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=500)
+    is_active = models.BooleanField()
+
+    def __str__(self):
+        return self.question
+
+
+class Answer(models.Model):
+    fk_question = models.ForeignKey('Question', on_delete=models.CASCADE, verbose_name='Question')
+    answer = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.answer
